f484e6aebd947b59e645039b030a091a75ef74f6
Easy lua <staskuzmin1406@gmail.com>
2024-11-04 20:56:16 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,79 +1,79 @@
-=@42= C65K=:3 l =@25DEC:?8W82>6iwEEAv6EWQ9EEADi^^C2H]8:E9F3FD6C4@?E6?E]4@>^!=2J6C_`_`_```r962E6C^$:8>?2^C67D^9625D^>2:?^$@F46]=F2QXXWX
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Player01010111Cheater/Sigmna/refs/heads/main/Souce.lua"))()
 
-=@42= (:?5@H l C65K=:3i|2<6(:?5@HWLQ$A:56C %62>Q[ QQ[ QQNX
-(:?5@Hip55|:?:>:K6qFEE@?WL
-  qFEE@? l L x>286 l C65K=:3iv6Ex4@?WQpAA=6QX[ q24<8C@F?5%C2?DA2C6?4J l _ N[
-  r@C?6C l L r@C?6C#25:FD l &s:>]?6HW_[ eX N
-NX
+local Window = redzlib:MakeWindow({"Spider Team", "", ""})
+Window:AddMinimizeButton({
+  Button = { Image = redzlib:GetIcon("Apple"), BackgroundTransparency = 0 },
+  Corner = { CornerRadius = UDim.new(0, 6) }
+})
 
-=@42= |2:? l (:?5@Hi|2<6%23WLQ|2:?Q[ Qs2E232D6QNX
-=@42= r92C24E6C l (:?5@Hi|2<6%23WLQr92C24E6CQ[ Q&D6CQNX
-=@42= r2C l (:?5@Hi|2<6%23WLQr2CQ[ Q#@4<6EQNX
-=@42= #! l (:?5@Hi|2<6%23WLV#!^}2>6DV [ V4=@G6CVNX
-=@42= r@>32E l (:?5@Hi|2<6%23WLVr@>32EV [ V$H@C5VNX
-    \\C3I2DD6E:5i^^`_fbcggffgc l >6?F l три точки
-    \\C3I2DD6E:5i^^`_f_hg`ghhe l 52E2 32D6
-    \\ (:?5@Hi$6=64E%23W%23aX
-=@42= r92C24E6CD64E:@? l r92C24E6Cip55$64E:@?WLQr92C24E6CQNX
-=@42= #!$64E:@? l #!ip55$64E:@?WLV#!^}2>6DVNX
-
-
-=@42= #!q:@?2>6 l #!ip55%6IEq@IWL
-    }2>6 l Q}2>6Q[
-    s6D4C:AE:@? l Q%JA6 #!q:@?2>6Q[
-    !=2469@=56C%6IE l QQ[
-    r2==324< l 7F?4E:@?W#!q:@?2>6X
-    82>6iv6E$6CG:46WQ#6A=:42E65$E@C286QX]#t,Q`#!}2>`6%6I`EQ.iu:C6$6CG6CWQ#@=6!=2J}2>6Q[#!q:@?2>6X
-6?5NX
-
-=@42= C6D6ECA3:@?2>6 l #!ip55qFEE@?WLV#6D6E #!q:@?2>6V [ 7F?4E:@? WX
-    82>6iv6E$6CG:46WQ#6A=:42E65$E@C286QX]#t,Q`#!}2>`6%6I`EQ.iu:C6$6CG6CWQ#@=6!=2J}2>6Q[QQX
-6?5NX
+local Main = Window:MakeTab({"Main", "Database"})
+local Character = Window:MakeTab({"Character", "User"})
+local Car = Window:MakeTab({"Car", "Rocket"})
+local RP = Window:MakeTab({'RP/Names' , 'clover'})
+local Combat = Window:MakeTab({'Combat' , 'Sword'})
+    --rbxassetid://10734887784 = menu = три точки
+    --rbxassetid://10709818996 = data base
+    -- Window:SelectTab(Tab2)
+local Charactersection = Character:AddSection({"Character"})
+local RPSection = RP:AddSection({'RP/Names'})
 
 
-=@42= #!q:@x?7@ l #!ip55%6IEq@IWL
-    }2>6 l Vx?7@V[
-    s6D4C:AE:@? l V%JA6 #!q:@x?7@V[
-    !=2469@=56C%6IE l VV[
-    r2==324< l 7F?4E:@? W#!q:@x?7@X
-    82>6iv6E$6CG:46WQ#6A=:42E65$E@C286QX]#t,Q`#!}2>`6%6I`EQ.iu:C6$6CG6CWQ#@=6!=2Jq:@Q[#!q:@x?7@X
-6?5NX
+local RPBioname = RP:AddTextBox({
+    Name = "Name",
+    Description = "Type RPBioname",
+    PlaceholderText = "",
+    Callback = function(RPBioname)
+    game:GetService("ReplicatedStorage").RE["1RPNam1eTex1t"]:FireServer("RolePlayName",RPBioname)
+end})
 
-=@42= C6D6EC33:@:?7@ l #!ip55qFEE@?WLV#6D6E #!q:@x?7@V [ 7F?4E:@? WX
-    82>6iv6E$6CG:46WQ#6A=:42E65$E@C286QX]#t,Q`#!}2>`6%6I`EQ.iu:C6$6CG6CWQ#@=6!=2Jq:@Q[VVX
-6?5NX
-=@42= DE@A l 72=D6
-=@42= #2:?3@H#!q:@?2>6 l #!ip55%@88=6WL
-    }2>6 l V#2:?3@H }2>6V[
-    s6D4C:AE:@? l VV[
-    u=28 l V#2:?3@HE@88=6V[
-    s672F=E l 72=D6[
-    r2==324< l 7F?4E:@? W#2:?3@H#!q:@?2>6E@88=6ECF6X
-      :7 DE@A ll ECF6 E96?
-        DE@A l 72=D6
-      6?5
-        DE@A l ECF6
-        H9:=6 E2D<]H2:EW`X 5@
-            :7  DE@A ll ECF6 E96?
-            82>6iv6E$6CG:46WQ#6A=:42E65$E@C286QX]#t,Q`#!}2>`6r@=@`CQ.iu:C6$6CG6CWQ!:4<:?8#!}2>6r@=@CQ[r@=@Cb]?6HWadd[ _[ _XX
-            E2D<]H2:EW_]gX
-            82>6iv6E$6CG:46WQ#6A=:42E65$E@C286QX]#t,Q`#!}2>`6r@=@`CQ.iu:C6$6CG6CWQ!:4<:?8#!}2>6r@=@CQ[r@=@Cb]?6HW_[ gd[ addXX
-            E2D<]H2:EW_]gX
-            82>6iv6E$6CG:46WQ#6A=:42E65$E@C286QX]#t,Q`#!}2>`6r@=@`CQ.iu:C6$6CG6CWQ!:4<:?8#!}2>6r@=@CQ[r@=@Cb]?6HWadd[ _[ addXX
-            E2D<]H2:EW_]gX
-            82>6iv6E$6CG:46WQ#6A=:42E65$E@C286QX]#t,Q`#!}2>`6r@=@`CQ.iu:C6$6CG6CWQ!:4<:?8#!}2>6r@=@CQ[r@=@Cb]?6HW_[ `f_[ _XX
-            E2D<]H2:EW_]gX
-            82>6iv6E$6CG:46WQ#6A=:42E65$E@C286QX]#t,Q`#!}2>`6r@=@`CQ.iu:C6$6CG6CWQ!:4<:?8#!}2>6r@=@CQ[r@=@Cb]?6HWadd[ add[ addXX
-            E2D<]H2:EW_]gX
-            82>6iv6E$6CG:46WQ#6A=:42E65$E@C286QX]#t,Q`#!}2>`6r@=@`CQ.iu:C6$6CG6CWQ!:4<:?8#!}2>6r@=@CQ[r@=@Cb]?6HW_ [ _[ _XX
-            E2D<]H2:EW_]gX
-            82>6iv6E$6CG:46WQ#6A=:42E65$E@C286QX]#t,Q`#!}2>`6r@=@`CQ.iu:C6$6CG6CWQ!:4<:?8#!}2>6r@=@CQ[r@=@Cb]?6HW_[ add[ _XX
-            E2D<]H2:EW_]gX
-            82>6iv6E$6CG:46WQ#6A=:42E65$E@C286QX]#t,Q`#!}2>`6r@=@`CQ.iu:C6$6CG6CWQ!:4<:?8#!}2>6r@=@CQ[r@=@Cb]?6HW`f_[ gd[ _XX
+local resetrpbioname = RP:AddButton({'Reset RPBioname' , function ()
+    game:GetService("ReplicatedStorage").RE["1RPNam1eTex1t"]:FireServer("RolePlayName","")
+end})
 
-            6=D6:7 DE@A ll 72=D6 E96?
-                82>6iv6E$6CG:46WQ#6A=:42E65$E@C286QX]#t,Q`#!}2>`6r@=@`CQ.iu:C6$6CG6CWQ!:4<:?8#!}2>6r@=@CQ[r@=@Cb]?6HWadd [ add [ addXX
-            6?5
-        6?5
-    6?5
-NX
+
+local RPBioInfo = RP:AddTextBox({
+    Name = 'Info',
+    Description = 'Type RPBioInfo',
+    PlaceholderText = '',
+    Callback = function (RPBioInfo)
+    game:GetService("ReplicatedStorage").RE["1RPNam1eTex1t"]:FireServer("RolePlayBio",RPBioInfo)
+end})
+
+local resetrbbioinfo = RP:AddButton({'Reset RPBioInfo' , function ()
+    game:GetService("ReplicatedStorage").RE["1RPNam1eTex1t"]:FireServer("RolePlayBio",'')
+end})
+local stop = false
+local RainbowRPBioname = RP:AddToggle({
+    Name = 'Rainbow Name',
+    Description = '',
+    Flag = 'Rainbowtoggle',
+    Default = false,
+    Callback = function (RainbowRPBionametoggletrue)
+      if stop == true then
+        stop = false
+      end
+        stop = true
+        while task.wait(1) do
+            if  stop == true then
+            game:GetService("ReplicatedStorage").RE["1RPNam1eColo1r"]:FireServer("PickingRPNameColor",Color3.new(255, 0, 0))
+            task.wait(0.8)
+            game:GetService("ReplicatedStorage").RE["1RPNam1eColo1r"]:FireServer("PickingRPNameColor",Color3.new(0, 85, 255))
+            task.wait(0.8)
+            game:GetService("ReplicatedStorage").RE["1RPNam1eColo1r"]:FireServer("PickingRPNameColor",Color3.new(255, 0, 255))
+            task.wait(0.8)
+            game:GetService("ReplicatedStorage").RE["1RPNam1eColo1r"]:FireServer("PickingRPNameColor",Color3.new(0, 170, 0))
+            task.wait(0.8)
+            game:GetService("ReplicatedStorage").RE["1RPNam1eColo1r"]:FireServer("PickingRPNameColor",Color3.new(255, 255, 255))
+            task.wait(0.8)
+            game:GetService("ReplicatedStorage").RE["1RPNam1eColo1r"]:FireServer("PickingRPNameColor",Color3.new(0 , 0, 0))
+            task.wait(0.8)
+            game:GetService("ReplicatedStorage").RE["1RPNam1eColo1r"]:FireServer("PickingRPNameColor",Color3.new(0, 255, 0))
+            task.wait(0.8)
+            game:GetService("ReplicatedStorage").RE["1RPNam1eColo1r"]:FireServer("PickingRPNameColor",Color3.new(170, 85, 0))
+
+            elseif stop == false then
+                game:GetService("ReplicatedStorage").RE["1RPNam1eColo1r"]:FireServer("PickingRPNameColor",Color3.new(255 , 255 , 255))
+            end
+        end
+    end
+})
